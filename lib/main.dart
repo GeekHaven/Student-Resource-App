6d2e905f28f2fb93fcef3b1fec8605b137d1bcc2
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-06-08 12:13:54 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Student Resource App')),
+        body: Center(child: Text('...'),),
+      ),
+      );
+  }
+}
