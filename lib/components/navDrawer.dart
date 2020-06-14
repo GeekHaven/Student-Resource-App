59a8caebfd85bc0395a23c9942739a060098bed1
Pranav Singhal <iit2019050@iiita.ac.in>
2020-06-14 18:17:47 +0530

    Added Feedback giving Feature and an Email sending utility.

--- lib/components/navDrawer.dart
+++ lib/components/navDrawer.dart
@@ -5,6 +5,7 @@ import 'package:studentresourceapp/pages/about.dart';
 import 'package:studentresourceapp/pages/home.dart';
 import 'package:studentresourceapp/pages/userdetailgetter.dart';
 import 'package:studentresourceapp/utils/signinutil.dart';
+import 'package:studentresourceapp/utils/emailutil.dart';
 
 class NavDrawer extends StatelessWidget {
   const NavDrawer({
@@ -60,6 +61,19 @@ class NavDrawer extends StatelessWidget {
               );
             }),
         NavItem(
+            title: 'Feedback',
+            iconData: Icons.feedback,
+            onPressed: () {
+//              Navigator.pop(context);
+//              Navigator.popUntil(context, ModalRoute.withName('/'));
+//              Navigator.of(context).push(
+//                MaterialPageRoute(builder: (BuildContext context) => Home()),
+//              );
+            Email email=Email();
+            email.launchEmail("studentresourceapp@gmail.com");
+            
+            }),
+        NavItem(
             title: 'About',
             iconData: Icons.info,
             onPressed: () {
@@ -69,6 +83,7 @@ class NavDrawer extends StatelessWidget {
                 builder: (BuildContext context) => About(),
               ));
             }),
+      
         NavItem(
           title: 'Sign Out',
           iconData: Icons.all_out,
