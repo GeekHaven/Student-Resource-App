7b4235b82e8eb25e94a316bcdbe1201e8475534c
yukta <iit2019052@iiita.ac.in>
2020-06-21 22:23:18 +0530

    Updated the info icon button

--- lib/pages/subject.dart
+++ lib/pages/subject.dart
@@ -21,51 +21,189 @@ class _SubjectState extends State<Subject> {
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('${widget.subjectCode}'),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.info_outline), onPressed: null)
-          ],
-          bottom: TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.note),
-              text: 'Material',
-            ),
-            Tab(
-              icon: Icon(Icons.edit),
-              text: 'Q-Paper',
-            ),
-            Tab(
-              icon: Icon(Icons.link),
-              text: 'Imp. Links',
-            )
-          ]),
-        ),
-        body: TabBarView(
-          children: [
-            StreamWidget(
-              widget: widget,
-              typeKey: 'Material',
-            ),
-            StreamWidget(
-              widget: widget,
-              typeKey: 'QuestionPapers',
-            ),
-            StreamWidget(
-              widget: widget,
-              typeKey: 'Important Links',
-            ),
-          ],
-        ),
-      ),
-    );
+  void handleClick(String value) {
+    switch (value) {
+      case 'Recommended Books':
+        recBooks(context);
+        break;
+      case 'Moderators':
+        modno(context);
+        break;
+    }
   }
-}
+    void modno(BuildContext context) {
+      showModalBottomSheet(context: context, builder: (builder) {
+        return Container(
+          child: Column(
+            children: <Widget>[
+              Center(child: Icon(Icons.arrow_drop_down),
+              ),
+              StreamBuilder(
+
+                stream: Firestore.instance
+                    .collection('Subjects')
+                    .document('${widget.semester}_${widget.subjectCode}')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  List<ListItem> messageWidget = [];
+                  if (snapshot.hasData) {
+                    List mod = snapshot.data['MODERATORS'];
+                    print(mod);
+                    for (int i = 0; i < mod.length; i++) {
+                      final ctnum = mod[i]['Contact Number'];
+                      final name = mod[i]['Name'];
+
+
+                      ListItem lis = ListItem(
+                          heading: 'Name: ', subheaading: name);
+
+
+                      messageWidget.add(lis);
+
+                      lis = ListItem(
+                          heading: 'Contact No. : ', subheaading: ctnum);
+                      messageWidget.add(lis);
+
+                      lis = ListItem(c: true);
+                      messageWidget.add(lis);
+                    }
+                  }
+                  return Expanded(
+                    child: ListView(
+
+                      children: messageWidget,
+                    ),
+                  )
+                  ;
+                },
+              ),
+
+            ],
+          ),
+        );
+      }
+      );
+    }
+
+    void recBooks(BuildContext context) {
+      showModalBottomSheet(context: context, builder: (builder) {
+        return Container(
+          child: Column(
+            children: <Widget>[
+              Center(child: Icon(Icons.arrow_drop_down),
+              ),
+              StreamBuilder(
+
+                stream: Firestore.instance
+                    .collection('Subjects')
+                    .document('${widget.semester}_${widget.subjectCode}')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  List<ListItem> messageWidget = [];
+                  if (snapshot.hasData) {
+                    List RecBooks = snapshot.data['Recommended Books'];
+                    print(RecBooks);
+
+                    for (int i = 0; i < RecBooks.length; i++) {
+                      final author = RecBooks[i]['Author'];
+                      final booktitle = RecBooks[i]['BookTitle'];
+                      final publication = RecBooks[i]['Publication'];
+
+//
+                      ListItem lis = ListItem(
+                          heading: '', subheaading: booktitle, b: true);
+                      messageWidget.add(lis);
+
+                      lis = ListItem
+                        (heading: 'Author : ', subheaading: author);
+                      messageWidget.add(lis);
+
+                      lis = ListItem(
+                          heading: 'Publication : ', subheaading: publication);
+
+                      messageWidget.add(lis);
+                      lis = ListItem(c: true);
+                      messageWidget.add(lis);
+                    }
+                  }
+                  return Expanded(
+                    child: ListView(
+//                    shrinkWrap: true,
+
+                      children: messageWidget,
+                    ),
+                  )
+                  ;
+                },
+              ),
+            ],
+          ),
+        );
+      }
+      );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('${widget.subjectCode}'),
+            actions: <Widget>[
+
+//            IconButton(icon: Icon(Icons.info_outline), onPressed: null)
+              PopupMenuButton<String>(
+                onSelected: handleClick,
+                icon: Icon
+                  (Icons.info_outline),
+                itemBuilder: (BuildContext context) {
+                  return {'Recommended Books', 'Moderators'}.map((
+                      String choice) {
+                    return PopupMenuItem<String>(
+                      value: choice,
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+              ),
+            ],
+            bottom: TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.note),
+                text: 'Material',
+              ),
+              Tab(
+                icon: Icon(Icons.edit),
+                text: 'Q-Paper',
+              ),
+              Tab(
+                icon: Icon(Icons.link),
+                text: 'Imp. Links',
+              )
+            ]),
+          ),
+          body: TabBarView(
+            children: [
+              StreamWidget(
+                widget: widget,
+                typeKey: 'Material',
+              ),
+              StreamWidget(
+                widget: widget,
+                typeKey: 'QuestionPapers',
+              ),
+              StreamWidget(
+                widget: widget,
+                typeKey: 'Important Links',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
 
 class StreamWidget extends StatelessWidget {
   const StreamWidget({Key key, @required this.widget, @required this.typeKey})
@@ -240,6 +378,96 @@ class StreamWidget extends StatelessWidget {
     }
   }
 }
+  class ListItem extends StatelessWidget {
+  @override
+
+  ListItem({this.heading,this.subheaading,this.b,this.c});
+
+  String heading;
+  String subheaading;
+  bool b=false;
+  bool c=false;
+
+
+  Widget build(BuildContext context) {
+
+  return Padding(
+
+  padding: EdgeInsets.all(5.0),
+
+  child: Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+  children: [
+  getWidget(),
+
+//          Text(
+//              heading
+//          ),
+//
+//          Text(
+//              subheaading
+//          ),
+  ],
+
+  ),
+  );
+
+  }
+  Widget getWidget(){
+
+
+  if(c==true)
+  {
+  return SizedBox(
+  height: 10.0,
+  width: 200.0,
+  child: Divider(
+  color: Colors.teal,
+  height: 50.0,
+  ),
+  );
+  }
+
+  if(b==true)
+  {
+  return Text(subheaading,
+  style: TextStyle(
+  color: Colors.teal,
+  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+  ),
+
+  );
+  }
+  else
+  {
+  return Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+  children: [
+
+
+  Text(
+  heading,
+  style: TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+  ),
+  ),
+
+  Text(
+  subheaading,
+  style: TextStyle(
+  fontSize: 20.0,
+  ),
+  ),
+  ],
+
+  );
+  }
+  }
+  }
 
 /*
 
