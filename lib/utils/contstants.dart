96877123f4233d97f6904670bd5fdbf00013f337
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-06-28 18:48:25 +0530

    Added UI Colors in constants.dart

--- lib/utils/contstants.dart
+++ lib/utils/contstants.dart
@@ -4,6 +4,16 @@ class Constants {
   static const USER_LOGGED_IN = 'is_user_logged_in';
   static const USER_DETAIL_OBJECT = 'user_object';
   static const COLLECTION_NAME_USER = 'userDetails';
+
+  /* Colors */
+  static const Color SMOKE = Color(0xffAFAFAF);                   // Tile separator color
+  static const Color MERCURY = Color(0xffE5E5E5);                 // Screen BackGround Color
+  static const Color STEEL = Color(0xff7A7A7A);                   // SubjectFull Name Color
+  static const Color SKYBLUE = Color(0xff79DBE8);                 // gradient end color
+  static const Color DARK_SKYBLUE = Color(0xff00BCD4);            // theme color
+  static const Color BLACK = Color(0xff000000);                   // subjectcode color
+  static const Color WHITE = Color(0xffFFFFFF);                   
+
   static const kTextFieldDecoration = InputDecoration(
   filled: true,
   fillColor: Colors.white,
