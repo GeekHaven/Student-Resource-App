161d33fd3c34e6d8ca60edcb98c51b37b3ce1da2
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-06-30 16:53:25 +0530

    Created Customm Loader using flareAnimation

---
+++ lib/components/custom_loader.dart
@@ -0,0 +1,16 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class CustomLoader extends StatelessWidget {
+  const CustomLoader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Center(child: 
+    FlareActor('assets/flareFiles/book_loader.flr', alignment: Alignment.center, animation: 'opening_closing')
+    );
+  }
+}
