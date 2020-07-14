f9d91bcca7f02d690d8a616adb43e505fc04f30c
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-07-14 19:55:24 +0530

    MInor changes

--- lib/pages/downloads.dart
+++ lib/pages/downloads.dart
@@ -47,12 +47,13 @@ class _DownloadsState extends State<Downloads> {
       });
     });
   }
+
   Future checkIfAdmin() async {
     final QuerySnapshot result =
         await Firestore.instance.collection('admins').getDocuments();
     final List<DocumentSnapshot> documents = result.documents;
-    documents.forEach((data){
-      if(data.documentID == userLoad.uid){
+    documents.forEach((data) {
+      if (data.documentID == userLoad.uid) {
         setState(() {
           admin = true;
         });
@@ -71,128 +72,139 @@ class _DownloadsState extends State<Downloads> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: NavDrawer(userData: userLoad, admin: admin,),
-        appBar: AppBar(
-          title: Text('Downloads'),
-        ),
-        body: Container(
-          child: file.length == 0
-              ? Center(child: Text('No downloads'))
-              : Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: ListView.builder(
-                          itemCount: file.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            String path = file[index].toString();
-                            String modifiedPath = modifyPath(path);
-                            String filename = modifiedPath
-                                .substring(modifiedPath.lastIndexOf('/') + 1);
-                            var arr = filename.split('_');
-                            int uniqueId = int.parse(arr[3]);
-                            int sem = int.parse(arr[0]);
-                            String subjectcode = arr[1];
-                            String typeKey = arr[2];
-                            String title = arr[4];
-                            return Padding(
-                              padding:
-                              const EdgeInsets.only(right: 16, left: 16, top: 12),
-                              child: Card(
-                                shadowColor: Color.fromRGBO(0, 0, 0, 0.75),
-                                elevation: 2,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: ListTile(
-                                  leading: IconButton(
-                                      icon: ImageIcon(
-                                          AssetImage('assets/svgIcons/preview.png')),
-                                      onPressed: () {
-                                        Navigator.of(context).push(MaterialPageRoute(
-                                            builder: (context) => PDFViewer(
-                                              sem: sem,
-                                              url: '',
-                                              subjectCode: subjectcode,
-                                              typeKey: typeKey,
-                                              uniqueID: uniqueId,
-                                              title: title,
-                                            )));
-                                      }),
-                                  title: Text(title,style: TextStyle(fontWeight: FontWeight.w600)),
-                                  subtitle: Text(
-                                      "$sem/$subjectcode/${typeKey == 'M' ? 'Material' : 'Q-Paper'}"),
-                                  trailing: IconButton(
-                                    icon: Icon(Icons.delete, color: Colors.red,size: 24),
+      drawer: NavDrawer(
+        userData: userLoad,
+        admin: admin,
+      ),
+      appBar: AppBar(
+        title: Text('Downloads'),
+      ),
+      body: Container(
+        child: file.length == 0
+            ? Center(child: Text('No downloads'))
+            : Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: file.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          String path = file[index].toString();
+                          String modifiedPath = modifyPath(path);
+                          String filename = modifiedPath
+                              .substring(modifiedPath.lastIndexOf('/') + 1);
+                          var arr = filename.split('_');
+                          int uniqueId = int.parse(arr[3]);
+                          int sem = int.parse(arr[0]);
+                          String subjectcode = arr[1];
+                          String typeKey = arr[2];
+                          String title = arr[4];
+                          return Padding(
+                            padding: const EdgeInsets.only(
+                                right: 16, left: 16, top: 12),
+                            child: Card(
+                              shadowColor: Color.fromRGBO(0, 0, 0, 0.75),
+                              elevation: 2,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: ListTile(
+                                leading: IconButton(
+                                    icon: ImageIcon(AssetImage(
+                                        'assets/svgIcons/preview.png')),
                                     onPressed: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-
-                                            contentPadding: EdgeInsets.all(32.0),
-                                            title: Text("⚠ Confirm Delete",
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(
+                                              builder: (context) => PDFViewer(
+                                                    sem: sem,
+                                                    url: '',
+                                                    subjectCode: subjectcode,
+                                                    typeKey: typeKey,
+                                                    uniqueID: uniqueId,
+                                                    title: title,
+                                                  )));
+                                    }),
+                                title: Text(title,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600)),
+                                subtitle: Text(
+                                    "$sem/$subjectcode/${typeKey == 'M' ? 'Material' : 'Q-Paper'}"),
+                                trailing: IconButton(
+                                  icon: Icon(Icons.delete,
+                                      color: Colors.red, size: 24),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          contentPadding: EdgeInsets.all(32.0),
+                                          title: Text(
+                                            "⚠ Confirm Delete",
                                             style: TextStyle(
                                               fontWeight: FontWeight.bold,
-                                            ),),
-                                            content: Text(
-                                                "Do you really want to delete this item?",
+                                            ),
+                                          ),
+                                          content: Text(
+                                            "Do you really want to delete this item?",
                                             style: TextStyle(
                                               fontSize: 20.0,
-
-                                            ),),
-                                            actions: [
-                                              FlatButton(
-                                                child: Text("Yes",
+                                            ),
+                                          ),
+                                          actions: [
+                                            FlatButton(
+                                              child: Text(
+                                                "Yes",
                                                 style: TextStyle(
                                                   fontSize: 20,
-                                                ),),
-                                                onPressed: () async {
-                                                  try {
-                                                    if (await File(modifiedPath)
-                                                        .exists()) {
-                                                      print('Path exists');
-                                                      File(modifiedPath)
-                                                          .deleteSync();
-                                                      List temp = file;
-                                                      temp.removeWhere(
-                                                          (element) =>
-                                                              element ==
-                                                              file[index]);
-                                                      setState(() {
-                                                        file = temp;
-                                                      });
-                                                    }
-                                                  } catch (err) {
-                                                    print(err);
-                                                  }
-                                                  Navigator.of(context).pop(true);
-                                                },
+                                                ),
                                               ),
-                                              FlatButton(
-                                                child: Text("No",
+                                              onPressed: () async {
+                                                try {
+                                                  if (await File(modifiedPath)
+                                                      .exists()) {
+                                                    print('Path exists');
+                                                    File(modifiedPath)
+                                                        .deleteSync();
+                                                    List temp = file;
+                                                    temp.removeWhere(
+                                                        (element) =>
+                                                            element ==
+                                                            file[index]);
+                                                    setState(() {
+                                                      file = temp;
+                                                    });
+                                                  }
+                                                } catch (err) {
+                                                  print(err);
+                                                }
+                                                Navigator.of(context).pop(true);
+                                              },
+                                            ),
+                                            FlatButton(
+                                              child: Text(
+                                                "No",
                                                 style: TextStyle(
-                                                    fontSize: 20,
-                                                      ),
-                                                    ),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop(true);
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        },
-                                      );
-                                    },
-                                  ),
+                                                  fontSize: 20,
+                                                ),
+                                              ),
+                                              onPressed: () {
+                                                Navigator.of(context).pop(true);
+                                              },
+                                            )
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
                                 ),
                               ),
-                            );
-                          }),
-                    )
-                  ],
-                ),
-        ),
-      );
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ),
+      ),
+    );
   }
 
   String modifyPath(String path) {
--- lib/pages/userdetailgetter.dart
+++ lib/pages/userdetailgetter.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:studentresourceapp/pages/signin.dart';
 import 'package:studentresourceapp/components/custom_dropdown.dart';
 import 'package:studentresourceapp/utils/contstants.dart';
+
 List<Color> _colors = [Constants.DARK_SKYBLUE, Constants.SKYBLUE];
 List<double> _stops = [0.0, 1.8];
 
-String college="IIITA";
+String college = "IIITA";
 int batch;
 String branch;
 int semester;
+
 class UserDetailGetter extends StatefulWidget {
   @override
   _UserDetailGetterState createState() => _UserDetailGetterState();
 }
 
 class _UserDetailGetterState extends State<UserDetailGetter> {
-
-
- // List<String> _college = ['IIITA', 'Others'];
- // String _selectedCollege;
+  // List<String> _college = ['IIITA', 'Others'];
+  // String _selectedCollege;
 
   List<String> _branches = ['IT', 'ITBI', 'ECE'];
 //  String _selectedBranch;
@@ -27,7 +27,7 @@ class _UserDetailGetterState extends State<UserDetailGetter> {
   //int _selectedSemester;
 
   List<int> _batches = [2020, 2019, 2018, 2017, 2016];
- //int _selectedBatch;
+  //int _selectedBatch;
 
   @override
   void initState() {
@@ -37,69 +37,80 @@ class _UserDetailGetterState extends State<UserDetailGetter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(14.0),
-                      child: Hero(
-                        tag: 'logo',
-                        child: Container(
-                            height: 180.0,
-                            child: Image.asset('assets/images/Logo.png')),
-                      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                          height: 180.0,
+                          child: Image.asset('assets/images/Logo.png')),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 28.0,top: 8.0,bottom: 8.0),
-                    child: Text(
-                      'Semester',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24,
-                          color: Colors.black),
-                    ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 28.0, top: 8.0, bottom: 8.0),
+                  child: Text(
+                    'Semester',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                        color: Colors.black),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-child: CustomDropdown(text: "",list: _semester,type: 1,),
-
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomDropdown(
+                    text: "",
+                    list: _semester,
+                    type: 1,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 28.0,top: 8.0,bottom: 8.0),
-                    child: Text(
-                      'Batch',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24,
-                          color: Colors.black),
-                    ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 28.0, top: 8.0, bottom: 8.0),
+                  child: Text(
+                    'Batch',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                        color: Colors.black),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomDropdown(text: "",list: _batches,type: 2,),
-
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomDropdown(
+                    text: "",
+                    list: _batches,
+                    type: 2,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 28.0,top: 8.0,bottom: 8.0),
-                    child: Text(
-                      'Branch',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24,
-                          color: Colors.black),
-                    ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 28.0, top: 8.0, bottom: 8.0),
+                  child: Text(
+                    'Branch',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                        color: Colors.black),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomDropdown(text: "",list: _branches,type: 3,),
-
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomDropdown(
+                    text: "",
+                    list: _branches,
+                    type: 3,
                   ),
+                ),
 
 //                  RaisedButton(
 //                      child: Text('Proceed'),
@@ -120,59 +131,57 @@ child: CustomDropdown(text: "",list: _semester,type: 1,),
                 Align(
                   alignment: Alignment.bottomRight,
                   child: GestureDetector(
-
-                    onTap: ()
-                    {
+                    onTap: () {
                       Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return SignIn(
-                                  college: college,
-                                  batch: batch,
-                                  branch: branch,
-                                  semester: semester);
-                            },
-                          ),
-                        );
-
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return SignIn(
+                                college: college,
+                                batch: batch,
+                                branch: branch,
+                                semester: semester);
+                          },
+                        ),
+                      );
                     },
                     child: Padding(
-
                       padding: EdgeInsets.all(36.0),
                       child: Container(
-
-                        height:50.0,
+                        height: 50.0,
                         width: 120.0,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(25),
                           gradient: LinearGradient(
                             colors: _colors,
                             stops: _stops,
-
-                          )
-                          ,
-                      ),
+                          ),
+                        ),
                         child: Padding(
                           padding: const EdgeInsets.all(12.0),
                           child: Row(
                             children: [
-
-                              Text("Next",style: TextStyle(color :Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold),),
+                              Text(
+                                "Next",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
                               Spacer(),
-                              Icon(Icons.arrow_forward_ios,color: Colors.white,
-
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.white,
                               )
                             ],
                           ),
                         ),
-
+                      ),
                     ),
-
+                  ),
                 ),
-                  ),),
-                ]),
-          ),
+              ]),
         ),
-      );
+      ),
+    );
   }
 }
