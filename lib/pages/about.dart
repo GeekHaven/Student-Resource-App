77f062cb514d559fee46834f4c8b245f16f5589f
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-07-16 13:07:52 +0530

    completed About Us

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,7 +1,12 @@
 import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:getwidget/components/accordian/gf_accordian.dart';
+import 'package:getwidget/components/avatar/gf_avatar.dart';
+import 'package:getwidget/components/list_tile/gf_list_tile.dart';
+import 'package:getwidget/size/gf_size.dart';
 import 'package:studentresourceapp/components/navDrawer.dart';
 import 'package:studentresourceapp/models/user.dart';
 import 'package:studentresourceapp/utils/contstants.dart';
@@ -48,102 +53,313 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(userData: userLoad, admin: admin,),
-      appBar: AppBar(title: Text('About')),
-      body: Padding(
-        padding: EdgeInsets.all(16),
+      drawer: NavDrawer(
+        userData: userLoad,
+        admin: admin,
+      ),
+      appBar: AppBar(title: Text('About Us')),
+      body: Container(
         child: SingleChildScrollView(
           child: Column(
-            children: [
-              Text(
-                'ABOUT',
-                style: TextStyle(fontSize: 40),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'About The App',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            children: <Widget>[
+              GFAccordion(
+                collapsedTitlebackgroundColor: Colors.transparent,
+                expandedTitlebackgroundColor: Colors.transparent,
+                contentbackgroundColor: Colors.transparent,
+                collapsedIcon: Text(''),
+                expandedIcon: Text(''),
+                titleChild: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.featherAlt,
+                    size: 32,
+                    color: Constants.DARK_SKYBLUE,
                   ),
-                  Text(
-                    'Hola Friends! We all know the importance of notes given out by the professors and our mighty toppers. Even when the \"Indian guy on YouTube\" fails to get things into our thick brains, it is these notes that come to our rescue. Now what if there was a central place where you would get all the magical notes and material to sail through these examinations, like a complete pro! Fret not, for we present to you, the SemBreaker App. Sounds fun, right? \n',
-                    style: TextStyle(fontSize: 15),
+                  title: Text(
+                    'About the App',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                  ),
+                ),
+                contentPadding: EdgeInsets.only(left: 80),
+                contentChild: Padding(
+                  padding: const EdgeInsets.only(right: 14),
+                  child: RichText(
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontFamily: 'Montserrat'),
+                      children: [
+                        TextSpan(
+                          text:
+                              'Hola Friends! We all know the importance of notes given out by the professors and our mighty toppers. Even when the \"Indian guy on YouTube\" fails to get things into our thick brains, it is these notes that come to our rescue. Now what if there was a central place where you would get all the magical notes and material to sail through these examinations, like a complete pro! Fret not, for we present to you, the ',
+                        ),
+                        TextSpan(
+                            text: 'SemBreaker App',
+                            style: TextStyle(fontWeight: FontWeight.w600)),
+                        TextSpan(text: '. Sounds fun, right?'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GFAccordion(
+                collapsedTitlebackgroundColor: Colors.transparent,
+                expandedTitlebackgroundColor: Colors.transparent,
+                contentbackgroundColor: Colors.transparent,
+                collapsedIcon: Text(''),
+                expandedIcon: Text(''),
+                titleChild: ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    color: Constants.DARK_SKYBLUE,
+                    size: 32,
+                  ),
+                  title: Text(
+                    'Features',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                  ),
+                ),
+                contentPadding: EdgeInsets.only(left: 80, right: 14),
+                contentChild: Column(
+                  children: <Widget>[
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontFamily: 'Montserrat'),
+                        children: [
+                          TextSpan(
+                            text: '\u2022 ',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                'One place access to all the important notes, study materials and previous year questions papers of the examinations.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontFamily: 'Montserrat'),
+                        children: [
+                          TextSpan(
+                            text: '\u2022 ',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                'Get the material across various semesters & courses.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontFamily: 'Montserrat'),
+                        children: [
+                          TextSpan(
+                            text: '\u2022 ',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                'Download the material that is important to you on your local phone storage.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontFamily: 'Montserrat'),
+                        children: [
+                          TextSpan(
+                            text: '\u2022 ',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                'If you have got the notes you want upload, contact the moderator of the course mentioned in the app.',
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    /*
+                    Expanded(
+                        child: Text(
+                            '\u2022 Get the material across various semesters & courses.\n')),
+                    Expanded(
+                        child: Text(
+                            '\u2022 Download the material that is important to you on your local phone storage.\n')),
+                    Expanded(
+                        child: Text(
+                            '\u2022 If you have got the notes you want upload, contact the moderator of the course mentioned in the app.\n'))
+                            */
+                  ],
+                ),
+              ),
+              GFAccordion(
+                collapsedTitlebackgroundColor: Colors.transparent,
+                expandedTitlebackgroundColor: Colors.transparent,
+                contentbackgroundColor: Colors.transparent,
+                collapsedIcon: Text(''),
+                expandedIcon: Text(''),
+                titleChild: ListTile(
+                  leading: Icon(
+                    Icons.settings_ethernet,
+                    color: Constants.DARK_SKYBLUE,
+                    size: 32,
+                  ),
+                  title: Text(
+                    'Contributors',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                  ),
+                ),
+                contentPadding: EdgeInsets.only(left: 80),
+                contentChild: Column(
+                  children: <Widget>[
+                    GFListTile(
+                        padding: EdgeInsets.only(
+                            right: 8, left: 0, bottom: 10, top: 10),
+                        margin: EdgeInsets.all(0),
+                        title: Text(
+                          'Avneesh Kumar',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        avatar: GFAvatar(
+                          backgroundColor: Constants.DARK_SKYBLUE,
+                          size: GFSize.MEDIUM + 2,
+                          child: GFAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                'https://avatars1.githubusercontent.com/u/54072374'),
+                          ),
+                        )),
+                    GFListTile(
+                        padding: EdgeInsets.only(
+                            right: 8, left: 0, bottom: 10, top: 10),
+                        margin: EdgeInsets.all(0),
+                        title: Text(
+                          'Pranav Singhal',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        avatar: GFAvatar(
+                          backgroundColor: Constants.DARK_SKYBLUE,
+                          size: GFSize.MEDIUM + 2,
+                          child: GFAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                'https://avatars2.githubusercontent.com/u/51447798'),
+                          ),
+                        )),
+                    GFListTile(
+                        padding: EdgeInsets.only(
+                            right: 8, left: 0, bottom: 10, top: 10),
+                        margin: EdgeInsets.all(0),
+                        title: Text(
+                          'Shourya',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        avatar: GFAvatar(
+                          backgroundColor: Constants.DARK_SKYBLUE,
+                          size: GFSize.MEDIUM + 2,
+                          child: GFAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                'https://avatars3.githubusercontent.com/u/58784199'),
+                          ),
+                        )),
+                    GFListTile(
+                        padding: EdgeInsets.only(
+                            right: 8, left: 0, bottom: 10, top: 10),
+                        margin: EdgeInsets.all(0),
+                        title: Text(
+                          'Tushar Kumar',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        avatar: GFAvatar(
+                          backgroundColor: Constants.DARK_SKYBLUE,
+                          size: GFSize.MEDIUM + 2,
+                          child: GFAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                'https://avatars1.githubusercontent.com/u/54072374'),
+                          ),
+                        )),
+                    GFListTile(
+                        padding: EdgeInsets.only(
+                            right: 8, left: 0, bottom: 10, top: 10),
+                        margin: EdgeInsets.all(0),
+                        title: Text(
+                          'Yukta Goplani',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        avatar: GFAvatar(
+                          backgroundColor: Constants.DARK_SKYBLUE,
+                          size: GFSize.MEDIUM + 2,
+                          child: GFAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                'https://avatars2.githubusercontent.com/u/59793009'),
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              GFAccordion(
+                collapsedTitlebackgroundColor: Colors.transparent,
+                expandedTitlebackgroundColor: Colors.transparent,
+                contentbackgroundColor: Colors.transparent,
+                collapsedIcon: Text(''),
+                expandedIcon: Text(''),
+                titleChild: ListTile(
+                  leading: ImageIcon(
+                    AssetImage('assets/images/geekhaven.png'),
+                    color: Constants.DARK_SKYBLUE,
+                    size: 32,
+                  ),
+                  title: Text(
+                    'About GeekHaven',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                  ),
+                ),
+                contentPadding: EdgeInsets.only(left: 80),
+                contentChild: Padding(
+                  padding: const EdgeInsets.only(right: 14),
+                  child: Text(
+                    'Hola Friends! We all know the importance of notes given out by the professors and our mighty toppers. Even when the \"Indian guy on YouTube\" fails to get things into our thick brains, it is these notes that come to our rescue. Now what if there was a central place where you would get all the magical notes and material to sail through these examinations, like a complete pro! Fret not, for we present to you, the SemBreaker App. Sounds fun, right?',
+                    style: TextStyle(fontSize: 14),
                     textAlign: TextAlign.justify,
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Features:',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Text(
-                    '1. One place access to all the important notes, study materials and previous year questions papers of the examinations. \n2. Get the material across various semesters & courses.\n3. Download the material that is important to you on your local phone storage.\n4. If you have got the notes you want upload, contact the moderator of the course mentioned in the app.',
-                    style: TextStyle(fontSize: 15),
-                    textAlign: TextAlign.justify,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Contributors:',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(),
-                      title: Text('Avneesh Kumar'),
-                      trailing: IconButton(icon: Icon(FontAwesomeIcons.github, color: Constants.DARK_SKYBLUE,), onPressed: (){}),
-                    ),
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(),
-                      title: Text('Yukta'),
-                      trailing: IconButton(icon: Icon(FontAwesomeIcons.github, color: Constants.DARK_SKYBLUE,), onPressed: (){}),
-                    ),
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(),
-                      title: Text('Pranav Singhal'),
-                      trailing: IconButton(icon: Icon(FontAwesomeIcons.github, color: Constants.DARK_SKYBLUE,), onPressed: (){}),
-                    ),
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(),
-                      title: Text('Shourya'),
-                      trailing: IconButton(icon: Icon(FontAwesomeIcons.github, color: Constants.DARK_SKYBLUE,), onPressed: (){}),
-                    ),
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(),
-                      title: Text('Tushar'),
-                      trailing: IconButton(icon: Icon(FontAwesomeIcons.dribbble, color: Constants.DARK_SKYBLUE,), onPressed: (){}),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'This app has been made in association with GeekHaven',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 10,
-                    fontStyle: FontStyle.italic),
+                ),
               ),
             ],
           ),
